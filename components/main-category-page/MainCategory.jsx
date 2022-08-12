43cb16b8d3823d71118970f45956997399e4749e
Safin Ahmed <safin.ahmed2000@gmail.com>
2022-08-12 23:57:07 +0600

    Added Title to main category page

--- components/main-category-page/MainCategory.jsx
+++ components/main-category-page/MainCategory.jsx
@@ -4,13 +4,16 @@ import { Container } from "@mui/material";
 import BackButton from "../UI/BackButton";
 import Categories from "../shared/Categories";
 import ProductsList from "../shared/ProductsList";
+import { capitalizeFirstLetter } from "../../utils/utils";
 
 const MainCategory = ({ products, categories, categoryName, parent }) => {
   return (
     <section style={{ marginTop: "7rem", paddingBottom: "5rem" }}>
       <Container maxWidth="lg">
         <BackButton />
-        <h1 style={{ textAlign: "center", fontSize: "48px" }}>{parent.name}</h1>
+        <h1 style={{ textAlign: "center", fontSize: "48px" }}>
+          {capitalizeFirstLetter(categoryName)}
+        </h1>
         <Categories categories={categories} baseUrl={`/shop/${categoryName}`} />
         <ProductsList products={products} />
       </Container>
